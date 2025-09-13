df14f86a01aa64ebec586d652e9c150b91bfd84c
Bartoszeq-tech <andrzejak.bartosz@gmail.com>
2025-09-14 01:40:07 +0200

    Krzyś now has a class and a fancy description

--- src/main/kotlin/pics/krzysiu/explosion/items/KrzysNiskiItem.kt
+++ src/main/kotlin/pics/krzysiu/explosion/items/KrzysNiskiItem.kt
@@ -1,15 +1,36 @@
 package pics.krzysiu.explosion.items
 
+import net.minecraft.component.type.TooltipDisplayComponent
 import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.item.tooltip.TooltipType
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
 import net.minecraft.util.Rarity
 import pics.krzysiu.explosion.ModItems
+import java.util.function.Consumer
 
-object KrzysNiskiItem {
-    val INSTANCE: Item = ModItems.register(
-        "krzys_jest_niski",
-        ::Item,
-        Item.Settings()
-            .maxCount(17)
-            .rarity(Rarity.EPIC)
-    )
+
+class KrzysNiskiItem(settings: Settings) : Item(settings) {
+    override fun appendTooltip(
+        stack: ItemStack?,
+        context: TooltipContext?,
+        displayComponent: TooltipDisplayComponent?,
+        textConsumer: Consumer<Text?>,
+        type: TooltipType?
+    ) {
+        textConsumer.accept(
+            Text.translatable("explosion.krzys_jest_niski.description").formatted(Formatting.GOLD)
+        )
+    }
+
+    companion object {
+        val INSTANCE: Item = ModItems.register(
+            "krzys_jest_niski",
+            ::KrzysNiskiItem,
+            Item.Settings()
+                .maxCount(17)
+                .rarity(Rarity.EPIC)
+        )
+    }
 }
